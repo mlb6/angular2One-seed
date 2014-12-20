3838ada100d7fefb1de4e33440f5a6d45c5e6d60
Martin Le Bas <lebas.martin@gmail.com>
2014-12-20 17:18:15 +1300

    Debug for gulp test-sauce process

--- scripts/test_on_sauce.sh
+++ scripts/test_on_sauce.sh
@@ -13,6 +13,14 @@ gulp build-dev
 #trap killServer EXIT
 
 gulp test-sauce &
+saucePid=$!
 #./node_modules/.bin/protractor protractor.travis.conf.js &
+
+while [ ! -f /tmp/waiting.debug ]; do
+  echo "waiting for $saucePid"
+  ps -efl | grep gulp
+  sleep 5
+done
 wait %1
+touch /tmp/waiting.debug
 gulp publish-coverage
