4c9d7e970a79754e42eacaf103832ea9334b439e
Martin Le Bas <lebas.martin@gmail.com>
2014-12-31 08:08:33 +1300

    Add protractor gulp tasks

---
+++ gulp/e2e-tests.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')();
+
+var browserSync = require('browser-sync');
+
+
+gulp.task('protractor', ['serve:dev:only', 'protractor:only']);
+gulp.task('protractor:prod', ['serve:prod:only', 'protractor:only']);
+
+
+gulp.task('protractor:only', ['webdriver-update'], function (done) {
+  var testFiles = [
+    'build/test/e2e/**/*.spec.js'
+  ];
+
+  gulp.src(testFiles)
+    .pipe($.protractor.protractor({
+      configFile: 'test/protractor.conf.js'
+    }))
+    .on('error', function (err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    })
+    .on('end', function () {
+      // Close browser sync server
+      browserSync.exit();
+      done();
+    });
+});
+
+
+// Downloads the selenium webdriver
+gulp.task('webdriver-update', $.protractor.webdriver_update);
+
+gulp.task('webdriver-standalone', $.protractor.webdriver_standalone);
